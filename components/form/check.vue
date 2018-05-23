8b2327e578de7568367a4d4de139f94e148cf034
Cesar Alvarez <alvarezc@sysmex.com>
2018-05-22 22:14:59 -0500

    Added check and etiqueta

---
+++ components/form/check.vue
@@ -0,0 +1,14 @@
+<template>
+    <v-checkbox :label="label" :required="required" :disabled="disabled"
+                  :value="internalValue" @input="update"></v-checkbox>
+</template>
+
+<script>
+  import base from './base'
+
+  export default {
+    name: 'AFormCheck',
+
+    mixins: [base]
+  }
+</script>
---
+++ components/form/etiqueta.vue
@@ -0,0 +1,13 @@
+<template>
+    <h3>{{label}}</h3>
+</template>
+
+<script>
+  import base from './base'
+
+  export default {
+    name: 'AFormEtiqueta',
+
+    mixins: [base]
+  }
+</script>
